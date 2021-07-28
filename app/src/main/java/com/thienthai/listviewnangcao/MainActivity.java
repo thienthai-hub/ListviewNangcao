f2949b77969adacea3646e6173f8dd7f52fc11f9
thienthai-hub <vohuyenthienthai94@gmail.com>
2021-07-28 16:42:16 +0700

    Cac buoc thuc hien

--- app/src/main/java/com/thienthai/listviewnangcao/MainActivity.java
+++ app/src/main/java/com/thienthai/listviewnangcao/MainActivity.java
@@ -9,48 +9,49 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    ListView lvTraiCay;
-    ArrayList<TraiCay> arrayListTraiCay;
-    TraiCayAdapter adapter;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        AnhSa();
+    //    B4
+        ListView lvTraiCay;
+        ArrayList<TraiCay> arrayListTraiCay;
+        TraiCayAdapter adapter;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            AnhSa();
 
-        adapter = new TraiCayAdapter(this, R.layout.dong_trai_cay,arrayListTraiCay);
-        lvTraiCay.setAdapter(adapter);
-    }
-    private void AnhSa(){
-        lvTraiCay = (ListView) findViewById(R.id.lvTraiCay);
-        arrayListTraiCay = new ArrayList<>();
+            adapter = new TraiCayAdapter(this, R.layout.dong_trai_cay,arrayListTraiCay);
+            lvTraiCay.setAdapter(adapter);
+        }
+        private void AnhSa(){
+            lvTraiCay = (ListView) findViewById(R.id.lvTraiCay);
+            arrayListTraiCay = new ArrayList<>();
 
-        arrayListTraiCay.add(new TraiCay("Ổi", "Ổi Cao Lãnh", R.drawable.traicay1));
-        arrayListTraiCay.add(new TraiCay("Bơ", "Ổi Cao Lãnh", R.drawable.traicay2));
-        arrayListTraiCay.add(new TraiCay("Khế", "Ổi Cao Lãnh", R.drawable.traicay3));
-        arrayListTraiCay.add(new TraiCay("Kiwi", "Ổi Cao Lãnh", R.drawable.traicay5));
-        arrayListTraiCay.add(new TraiCay("khớm", "Ổi Cao Lãnh", R.drawable.traicay6));
-        arrayListTraiCay.add(new TraiCay("Dừa", "Ổi Cao Lãnh", R.drawable.traicay7));
-        arrayListTraiCay.add(new TraiCay("Nho mỹ", "Ổi Cao Lãnh", R.drawable.traicay8));
-        arrayListTraiCay.add(new TraiCay("Ổi", "Ổi Cao Lãnh", R.drawable.traicay1));
-        arrayListTraiCay.add(new TraiCay("Bơ", "Ổi Cao Lãnh", R.drawable.traicay2));
-        arrayListTraiCay.add(new TraiCay("Khế", "Ổi Cao Lãnh", R.drawable.traicay3));
-        arrayListTraiCay.add(new TraiCay("Kiwi", "Ổi Cao Lãnh", R.drawable.traicay5));
-        arrayListTraiCay.add(new TraiCay("khớm", "Ổi Cao Lãnh", R.drawable.traicay6));
-        arrayListTraiCay.add(new TraiCay("Dừa", "Ổi Cao Lãnh", R.drawable.traicay7));
-        arrayListTraiCay.add(new TraiCay("Nho mỹ", "Ổi Cao Lãnh", R.drawable.traicay8));
-        arrayListTraiCay.add(new TraiCay("Ổi", "Ổi Cao Lãnh", R.drawable.traicay1));
-        arrayListTraiCay.add(new TraiCay("Bơ", "Ổi Cao Lãnh", R.drawable.traicay2));
-        arrayListTraiCay.add(new TraiCay("Khế", "Ổi Cao Lãnh", R.drawable.traicay3));
-        arrayListTraiCay.add(new TraiCay("Kiwi", "Ổi Cao Lãnh", R.drawable.traicay5));
-        arrayListTraiCay.add(new TraiCay("khớm", "Ổi Cao Lãnh", R.drawable.traicay6));
-        arrayListTraiCay.add(new TraiCay("Dừa", "Ổi Cao Lãnh", R.drawable.traicay7));
-        arrayListTraiCay.add(new TraiCay("Nho mỹ", "Ổi Cao Lãnh", R.drawable.traicay8));
-        arrayListTraiCay.add(new TraiCay("Ổi", "Ổi Cao Lãnh", R.drawable.traicay1));
-        arrayListTraiCay.add(new TraiCay("Bơ", "Ổi Cao Lãnh", R.drawable.traicay2));
-        arrayListTraiCay.add(new TraiCay("Khế", "Ổi Cao Lãnh", R.drawable.traicay3));
-        //delte
-    }
+            arrayListTraiCay.add(new TraiCay("Ổi", "Ổi Cao Lãnh", R.drawable.traicay1));
+            arrayListTraiCay.add(new TraiCay("Bơ", "Ổi Cao Lãnh", R.drawable.traicay2));
+            arrayListTraiCay.add(new TraiCay("Khế", "Ổi Cao Lãnh", R.drawable.traicay3));
+            arrayListTraiCay.add(new TraiCay("Kiwi", "Ổi Cao Lãnh", R.drawable.traicay5));
+            arrayListTraiCay.add(new TraiCay("khớm", "Ổi Cao Lãnh", R.drawable.traicay6));
+            arrayListTraiCay.add(new TraiCay("Dừa", "Ổi Cao Lãnh", R.drawable.traicay7));
+            arrayListTraiCay.add(new TraiCay("Nho mỹ", "Ổi Cao Lãnh", R.drawable.traicay8));
+            arrayListTraiCay.add(new TraiCay("Ổi", "Ổi Cao Lãnh", R.drawable.traicay1));
+            arrayListTraiCay.add(new TraiCay("Bơ", "Ổi Cao Lãnh", R.drawable.traicay2));
+            arrayListTraiCay.add(new TraiCay("Khế", "Ổi Cao Lãnh", R.drawable.traicay3));
+            arrayListTraiCay.add(new TraiCay("Kiwi", "Ổi Cao Lãnh", R.drawable.traicay5));
+            arrayListTraiCay.add(new TraiCay("khớm", "Ổi Cao Lãnh", R.drawable.traicay6));
+            arrayListTraiCay.add(new TraiCay("Dừa", "Ổi Cao Lãnh", R.drawable.traicay7));
+            arrayListTraiCay.add(new TraiCay("Nho mỹ", "Ổi Cao Lãnh", R.drawable.traicay8));
+            arrayListTraiCay.add(new TraiCay("Ổi", "Ổi Cao Lãnh", R.drawable.traicay1));
+            arrayListTraiCay.add(new TraiCay("Bơ", "Ổi Cao Lãnh", R.drawable.traicay2));
+            arrayListTraiCay.add(new TraiCay("Khế", "Ổi Cao Lãnh", R.drawable.traicay3));
+            arrayListTraiCay.add(new TraiCay("Kiwi", "Ổi Cao Lãnh", R.drawable.traicay5));
+            arrayListTraiCay.add(new TraiCay("khớm", "Ổi Cao Lãnh", R.drawable.traicay6));
+            arrayListTraiCay.add(new TraiCay("Dừa", "Ổi Cao Lãnh", R.drawable.traicay7));
+            arrayListTraiCay.add(new TraiCay("Nho mỹ", "Ổi Cao Lãnh", R.drawable.traicay8));
+            arrayListTraiCay.add(new TraiCay("Ổi", "Ổi Cao Lãnh", R.drawable.traicay1));
+            arrayListTraiCay.add(new TraiCay("Bơ", "Ổi Cao Lãnh", R.drawable.traicay2));
+            arrayListTraiCay.add(new TraiCay("Khế", "Ổi Cao Lãnh", R.drawable.traicay3));
+            //delte
+        }
 
 
 }
--- app/src/main/java/com/thienthai/listviewnangcao/TraiCay.java
+++ app/src/main/java/com/thienthai/listviewnangcao/TraiCay.java
@@ -1,41 +1,41 @@
 package com.thienthai.listviewnangcao;
+//b2
+    public class TraiCay {
+        private String Ten;
+        private String MoTa;
+        private int Hinh;
 
-public class TraiCay {
-    private String Ten;
-    private String MoTa;
-    private int Hinh;
+        //contructor
 
-    //contructor
+        public TraiCay(String ten, String moTa, int hinh) {
+            Ten = ten;
+            MoTa = moTa;
+            Hinh = hinh;
+        }
 
-    public TraiCay(String ten, String moTa, int hinh) {
-        Ten = ten;
-        MoTa = moTa;
-        Hinh = hinh;
-    }
+        //getSet
 
-    //getSet
+        public String getTen() {
+            return Ten;
+        }
 
-    public String getTen() {
-        return Ten;
-    }
+        public void setTen(String ten) {
+            Ten = ten;
+        }
 
-    public void setTen(String ten) {
-        Ten = ten;
-    }
+        public String getMoTa() {
+            return MoTa;
+        }
 
-    public String getMoTa() {
-        return MoTa;
-    }
+        public void setMoTa(String moTa) {
+            MoTa = moTa;
+        }
 
-    public void setMoTa(String moTa) {
-        MoTa = moTa;
-    }
+        public int getHinh() {
+            return Hinh;
+        }
 
-    public int getHinh() {
-        return Hinh;
-    }
-
-    public void setHinh(int hinh) {
-        Hinh = hinh;
-    }
+        public void setHinh(int hinh) {
+            Hinh = hinh;
+        }
 }
