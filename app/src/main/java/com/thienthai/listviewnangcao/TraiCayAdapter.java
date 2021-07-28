3962144cdc78e5f7330db2b0ec003cf0674068e6
thienthai-hub <vohuyenthienthai94@gmail.com>
2021-07-28 16:36:15 +0700

    Cac buoc thu hien

--- app/src/main/java/com/thienthai/listviewnangcao/TraiCayAdapter.java
+++ app/src/main/java/com/thienthai/listviewnangcao/TraiCayAdapter.java
@@ -14,60 +14,60 @@ import java.util.List;
 //B3- custom Adapter để show ra màng hình
 public class  TraiCayAdapter extends BaseAdapter {
 
-    private Context context;
-    private int layout;
-    private List<TraiCay> traiCayList;
+        private Context context;
+        private int layout;
+        private List<TraiCay> traiCayList;
 
-    public TraiCayAdapter(Context context, int layout, List<TraiCay> traiCayList) {
-        this.context = context;
-        this.layout = layout;
-        this.traiCayList = traiCayList;
-    }
-
-    @Override
-    public int getCount() {
-        return traiCayList.size();
-    }
-
-    @Override
-    public Object getItem(int position) {
-        return null;
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return 0;
-    }
-    //tap class Hoder
-    private class viewHolder{
-        TextView txtTen, txtDes;
-        ImageView hinhAnh;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-
-        viewHolder holder;
-        if (convertView == null){
-            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = inflater.inflate(layout,null);
-
-            holder = new viewHolder();
-            //anhSa
-            holder.txtDes = (TextView) convertView.findViewById(R.id.textViewTen);
-            holder.txtTen = (TextView) convertView.findViewById(R.id.textViewDes);
-            holder.hinhAnh = (ImageView) convertView.findViewById(R.id.imageView);
-            convertView.setTag(holder);
-        }else {
-            holder = (viewHolder) convertView.getTag();
+        public TraiCayAdapter(Context context, int layout, List<TraiCay> traiCayList) {
+            this.context = context;
+            this.layout = layout;
+            this.traiCayList = traiCayList;
         }
 
-        //gáng giá trị.
-        TraiCay traiCay = traiCayList.get(position);
-        holder.txtTen.setText(traiCay.getTen());
-        holder.txtDes.setText(traiCay.getMoTa());
-        holder.hinhAnh.setImageResource(traiCay.getHinh());
+        @Override
+        public int getCount() {
+            return traiCayList.size();
+        }
 
-        return convertView;
-    }
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+        //tap class Hoder
+        private class viewHolder{
+            TextView txtTen, txtDes;
+            ImageView hinhAnh;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            viewHolder holder;
+            if (convertView == null){
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = inflater.inflate(layout,null);
+
+                holder = new viewHolder();
+                //anhSa
+                holder.txtDes = (TextView) convertView.findViewById(R.id.textViewTen);
+                holder.txtTen = (TextView) convertView.findViewById(R.id.textViewDes);
+                holder.hinhAnh = (ImageView) convertView.findViewById(R.id.imageView);
+                convertView.setTag(holder);
+            }else {
+                holder = (viewHolder) convertView.getTag();
+            }
+
+            //gáng giá trị.
+            TraiCay traiCay = traiCayList.get(position);
+            holder.txtTen.setText(traiCay.getTen());
+            holder.txtDes.setText(traiCay.getMoTa());
+            holder.hinhAnh.setImageResource(traiCay.getHinh());
+
+            return convertView;
+        }
 }
